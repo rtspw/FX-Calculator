ca0c23d8c287cf2b38baf2f26a3cc190afabdb55
rtspw <richard.cx.tang@gmail.com>
2019-05-28 18:51:22 -0700

    Wrote tests and framework for notation converter class

---
+++ src/main/java/com/rtspw/calculator/NotationConverter.java
@@ -0,0 +1,11 @@
+package com.rtspw.calculator;
+
+class NotationConverter {
+    static String infixToPostfix(String equation) {
+        return "";
+    }
+
+    static String UTF16ToASCIIOperators(String equation) {
+        return "";
+    }
+}
