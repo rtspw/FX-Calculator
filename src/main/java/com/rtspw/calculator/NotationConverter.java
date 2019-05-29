cb5ada5c7606d0e8d8790b14e192e457973304bd
rtspw <richard.cx.tang@gmail.com>
2019-05-28 19:22:04 -0700

    Fixed assertEqual order and implemented UTF16ToASCIIOperators

--- src/main/java/com/rtspw/calculator/NotationConverter.java
+++ src/main/java/com/rtspw/calculator/NotationConverter.java
@@ -1,11 +1,29 @@
 package com.rtspw.calculator;
 
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
 class NotationConverter {
+
+    private static final HashMap<Character, Character> operatorMap;
+    static {
+        operatorMap = new HashMap<>();
+        operatorMap.put('×', '*');
+        operatorMap.put('÷', '/');
+        operatorMap.put('±', '-');
+
+    }
+
     static String infixToPostfix(String equation) {
         return "";
     }
 
     static String UTF16ToASCIIOperators(String equation) {
-        return "";
+        return equation.codePoints()
+                .mapToObj(c -> (char)c)
+                .map(c -> operatorMap.getOrDefault(c, c))
+                .map(Object::toString)
+                .collect(Collectors.joining());
+
     }
 }
--- src/test/java/com/rtspw/calculator/NotationConverterTest.java
+++ src/test/java/com/rtspw/calculator/NotationConverterTest.java
@@ -8,43 +8,43 @@ public class NotationConverterTest {
 
     @Test
     public void operatorConversionTest() {
-        assertEquals(NotationConverter.UTF16ToASCIIOperators("3 ÷ 2 × ±1"), "3 / 2 * -1");
+        assertEquals("3 / 2 * -1", NotationConverter.UTF16ToASCIIOperators("3 ÷ 2 × ±1"));
     }
 
     @Test
     @Ignore("Still in progress")
     public void BasicOperatorsTest() {
-        assertEquals(NotationConverter.infixToPostfix("3 + 2"), "3 2 +");
-        assertEquals(NotationConverter.infixToPostfix("2 + 3"), "2 3 +");
-        assertEquals(NotationConverter.infixToPostfix("(3) + (2)"), "3 2 +");
-        assertEquals(NotationConverter.infixToPostfix("(3 + 2)"), "3 2 +");
-        assertEquals(NotationConverter.infixToPostfix("5 - 4 + 3"), "5 4 - 3 +");
-        assertEquals(NotationConverter.infixToPostfix("3 ÷ 2 ÷ 1"), "3 2 ÷ 1 ÷");
-        assertEquals(NotationConverter.infixToPostfix("3 ÷ (2 × 1)"), "3 2 1 × ÷");
-        assertEquals(NotationConverter.infixToPostfix("(3 ÷ 2) × 1"), "3 2 ÷ 1 ×");
+        assertEquals("3 2 +", NotationConverter.infixToPostfix("3 + 2"));
+        assertEquals("2 3 +", NotationConverter.infixToPostfix("2 + 3"));
+        assertEquals("3 2 +", NotationConverter.infixToPostfix("(3) + (2)"));
+        assertEquals("3 2 +", NotationConverter.infixToPostfix("(3 + 2)"));
+        assertEquals("5 4 - 3 +", NotationConverter.infixToPostfix("5 - 4 + 3"));
+        assertEquals("3 2 / 1 /", NotationConverter.infixToPostfix("3 / 2 / 1"));
+        assertEquals("3 2 1 * /", NotationConverter.infixToPostfix("3 / (2 * 1)"));
+        assertEquals("3 2 / 1 *", NotationConverter.infixToPostfix("(3 / 2) * 1"));
     }
 
     @Test
     @Ignore("Still in progress")
     public void NegativeNumbersTest() {
-        assertEquals(NotationConverter.infixToPostfix("3 + -2"), "3 -2 +");
-        assertEquals(NotationConverter.infixToPostfix("-3 - -2"), "-3 -2 -");
+        assertEquals("3 -2 +", NotationConverter.infixToPostfix("3 + -2"));
+        assertEquals("-3 -2 -", NotationConverter.infixToPostfix("-3 - -2"));
     }
 
     @Test
     @Ignore("Still in progress")
     public void orderOfOperationsTest() {
-        assertEquals(NotationConverter.infixToPostfix("3 + 2 × 4"), "3 2 4 × +");
-        assertEquals(NotationConverter.infixToPostfix("3 × 2 × 4"), "3 2 × 4 ×");
-        assertEquals(NotationConverter.infixToPostfix("3 × 2 + 4"), "3 2 × 4 +");
-        assertEquals(NotationConverter.infixToPostfix("1 × 2^(3 + 4)"), "1 2 3 4 + ^ ×");
+        assertEquals("3 2 4 * +", NotationConverter.infixToPostfix("3 + 2 * 4"));
+        assertEquals("3 2 * 4 *", NotationConverter.infixToPostfix("3 * 2 * 4"));
+        assertEquals("3 2 * 4 +", NotationConverter.infixToPostfix("3 * 2 + 4"));
+        assertEquals("1 2 3 4 + ^ *", NotationConverter.infixToPostfix("1 * 2^(3 + 4)"));
     }
 
     @Test
     @Ignore("Still in progress")
     public void unaryFunctionTest() {
-        assertEquals(NotationConverter.infixToPostfix("sin(3)"), "3 sin");
-        assertEquals(NotationConverter.infixToPostfix("2 - cos(5 + 3)"), "2 5 3 + cos -");
+        assertEquals("3 sin", NotationConverter.infixToPostfix("sin(3)"));
+        assertEquals("2 5 3 + cos -", NotationConverter.infixToPostfix("2 - cos(5 + 3)"));
 
     }
 }
