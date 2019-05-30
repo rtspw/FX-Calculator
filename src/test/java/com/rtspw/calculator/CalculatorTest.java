03e2503b910a44ff74d88f2ef64c4eb2775d2323
rtspw <richard.cx.tang@gmail.com>
2019-05-29 19:21:30 -0700

    Added more calculator tests

--- src/test/java/com/rtspw/calculator/CalculatorTest.java
+++ src/test/java/com/rtspw/calculator/CalculatorTest.java
@@ -17,6 +17,10 @@ public class CalculatorTest {
 
     @Test
     public void unaryFunctionCalculationTest() {
-        assertEquals("0.141", Calculator.parseInfixEquation("sin(3)"));
+        assertEquals(String.format("%.10f", Math.sin(3)), Calculator.parseInfixEquation("sin(3)"));
+        assertEquals(String.format("%.10f", Math.sin(Math.cos(3))), Calculator.parseInfixEquation("sin(cos(3))"));
+        assertEquals(String.format("%.10f", Math.log(2 * 3) / Math.tan(2 * 4)),
+                Calculator.parseInfixEquation("log(2 * 3) / tan(2 * 4)"));
     }
+
 }
