c1e2a54805f0682f3e1e5f511f2a1ebc63c85e2e
rtspw <richard.cx.tang@gmail.com>
2019-05-29 19:15:32 -0700

    Removed primary page from project

--- src/main/java/com/rtspw/calculator/App.java
+++ src/main/java/com/rtspw/calculator/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"));
+        scene = new Scene(loadFXML("calculator"));
         stage.setMinHeight(400);
         stage.setMinWidth(500);
         stage.setScene(scene);
