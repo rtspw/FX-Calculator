132dfc15126a8a33018c746ecb1e68a1ba6b4470
rtspw <richard.cx.tang@gmail.com>
2019-06-02 22:13:41 -0700

    Very very minor project cleanup

--- src/main/java/com/rtspw/calculator/CalculatorController.java
+++ src/main/java/com/rtspw/calculator/CalculatorController.java
@@ -3,7 +3,6 @@ package com.rtspw.calculator;
 import javafx.fxml.FXML;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.LineChart;
-import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
